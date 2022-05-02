5901af6e173a60eeb3cbe5082fde163eb8090515
pd-ualr <pdeano@ualr.edu>
2022-05-02 19:15:11 +0000

    JS carousel demo

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,17 @@
+<!-- Alternate Configuration Options Here - https://accessible360.github.io/accessible-slick/#demos -->
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 100,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: false,
+      autoplaySpeed: 2000
+    });
+
+
+});
